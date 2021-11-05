438d2f10d46d1c560683a97c50f2bcb1483a2b4c
breno.afb <breno.afb@gmail.com>
2021-11-05 11:01:30 -0300

    Fix example script

--- script.lisp
+++ script.lisp
@@ -4,7 +4,7 @@
 (define ((caddr any) (x list))
   (car (cdr (cdr x))))
 
-(define ((caar any (x list))
+(define ((caar any) (x list))
   (car (car x)))
 
 (define ((cadar any) (x list))
@@ -14,113 +14,88 @@
   (cond (x (cond (y true)))
 	(true false)))
 
-(define (or x y)
-  (cond (x '#t)
-	(y '#t)
-	('#t '())))
+(define ((or bool) (x bool) (y bool))
+  (cond (x true)
+	(y true)
+	(true false)))
 
-(define (not x)
-  (cond (x '())
-	('#t '#t)))
+(define ((not bool) (x bool))
+  (cond (x false)
+	(true true)))
 
-(define (atom? x)
-  (cond ((eq? (type x) 'atom) '#t)
-	('#t '())))
+(define ((atom? bool) (x any))
+  (cond ((eq? (type x) 'atom) true)
+	(true false)))
 
-(define (zip x y)
+(define ((zip list) (x list) (y list))
   (cond ((and (null? x) (null? y)) '())
 	((and (not (atom? x)) (not (atom? y)))
-	 (cons (list (car x) (car y))
+	 (cons (mklist (car x) (car y))
 	       (zip (cdr x) (cdr y))))))
 
-(define (fst pair)
+(define ((fst any) (pair list))
   (car pair))
 
-(define (snd pair)
+(define ((snd any) (pair list))
   (cadr pair))
 
-(define (map f xs)
+(define ((map list) (f any) (xs list))
   (cond ((null? xs) '())
-	('#t (cons (f (car xs))
+	(true (cons (f (car xs))
 		   (map f (cdr xs))))))
 
-(define (filter p xs)
+(define ((filter list) (p any) (xs list))
   (cond ((null? xs) '())
 	((p (car xs))
 	 (cons (car xs)
 	       (filter p (cdr xs))))
-	('#t (filter p (cdr xs)))))
+	(true (filter p (cdr xs)))))
 
-(define (even? x)
-  (cond ((eq? (mod x 2) 0) '#t)
-	('#t '())))
+(define ((even? bool) (x int))
+  (cond ((eq? (mod x 2) 0) true)
+	(true false)))
 
-(define (foldr f x0 xs)
+(define ((foldr any) (f any) (x0 any) (xs list))
   (cond ((null? xs) x0)
-	('#t (f (car xs)
+	(true (f (car xs)
 		(foldr f x0 (cdr xs))))))
 
-(define (sum.i xs)
+(define ((sum.i int) (xs list))
   (foldr +.i 0 xs))
 
-(define (prod.i xs)
+(define ((prod.i int) (xs list))
   (foldr *.i 1 xs))
 
-(define (all p xs)
-  (foldr and '#t (map p xs)))
+(define ((all bool) (p any) (xs list))
+  (foldr and true (map p xs)))
 
-(define (any p xs)
+(define ((any bool) (p any) (xs list))
   (foldr or '() (map p xs)))
 
-(define (assoc x y)
+(define ((assoc any) (x any) (y list))
   (cond ((eq? (caar y) x) (cadar y))
-	('#t (assoc x (cdr y)))))
+	(true (assoc x (cdr y)))))
 
-(define (elem? x xs)
-  (cond ((null? xs) '())
-	((eq? (car xs) x) '#t)
-	('#t (elem? x (cdr xs)))))
+(define ((elem? bool) (x any) (xs list))
+  (cond ((null? xs) false)
+	((eq? (car xs) x) true)
+	(true (elem? x (cdr xs)))))
 
-(define (replace pairs xs)
+(define ((replace list) (pairs list) (xs list))
   (foldr
     (lambda (x acc)
       (cond ((eq? (type x) 'list)
 	     (cons (replace pairs x) acc))
-	    ('#t
+	    (true
 	     (cond ((elem? x (map fst pairs))
 		    (cons (assoc x pairs) acc))
-		   ('#t (cons x acc))))))
+		   (true (cons x acc))))))
     '()
     xs))
 
-(define (num-op-template x y)
-   (cond ((and (eq? (type x) 'double)
-	       (eq? (type y) 'double))
-	  (__D_OP__ x y))
-	 ((and (eq? (type x) 'int)
-	       (eq? (type y) 'int))
-	  (__I_OP__ x y))
-	 ('#t '())))
-
-(define (mk-num-op double-op int-op)
-  (replace (zip '(__D_OP__ __I_OP__) (list double-op int-op))
-	   num-op-template))
-
-(define +
-  (mk-num-op '+.f '+.i))
-
-(define *
-  (mk-num-op '*.f '*.i))
-
-(define -
-  (mk-num-op '-.f '-.i))
-
-(define /
-  (mk-num-op '/.f '/.i))
-
-(define (length xs)
+(define ((length int) (xs list))
   (cond ((null? xs) 0)
-	('#t (+ 1 (length (cdr xs))))))
+	(true (+.i 1 (length (cdr xs))))))
 
 
 (cadr '(1 2 3))
@@ -135,38 +110,38 @@
 (cadar '((1 2) 2))
 ; 2
 
-(and '#t '#t)
+(and true true)
 ; #t
 
-(and '#t '())
+(and true false)
 ; '()
 
-(and '() '#t)
+(and false true)
 ; '()
 
-(and '() '())
+(and false false)
 ; '()
 
-(or '#t '#t)
-; '#t
+(or true true)
+; true
 
-(or '#t '())
-; '#t
+(or true false)
+; true
 
-(or '() '#t)
-; '#t
+(or false true)
+; true
 
-(or '() '())
+(or false false)
 ; '()
 
 
-(not '#t)
+(not true)
 ; '()
 
-(not '())
-; '#t
+(not false)
+; true
 
-(define pairs (zip '(0 1 2 3) '(a b c d)))
+(define (pairs list) (zip '(0 1 2 3) '(a b c d)))
 
 pairs
 
@@ -194,7 +169,7 @@ pairs
 
 (any even? '(1 3 5 7 9))
 
-(define dict
+(define (dict list)
   '((a "entry for a")
     (b "entry for b")
     (c "entry for c")
@@ -204,25 +179,10 @@ pairs
 (assoc 'b dict)
 (assoc 'c dict)
 (assoc 'd dict)
-(assoc 'e dict)
 
 (replace '((hot cold)) '(it is hot))
 
 (replace '((small big) (simple complicated)) '(this (is a (very small and (simple)) structure)))
-
-(+ 1 2)
-
-(+ (neg 1) 2)
-
-(+ 1.1 2.2)
-
-(* 1 2)
-
-(* 1.1 2.2)
-
-(- 2 1)
-
-(- 2.127 1.23)
 
 (length '())
 (length '(1 2 3 4))
